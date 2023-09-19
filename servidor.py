bcbba8a3938b03039c34555be6d43f7822b24473
Thiago Sanches <thiago.sanches0703@gmail.com>
2023-09-18 23:58:09 -0300

    ajustando timeout

--- servidor.py
+++ servidor.py
@@ -28,7 +28,7 @@ def handle_client(client_socket, base_dir):
         # a serem lidos de uma vez.
         # No caso dessa linha, no máximo 1024 bytes de dados do cliente estao sendo lidos
         # de uma só vez.
-        # request_text armazena todas as informacoes da requisicao GET /index.htmla HTTP/1.1, host, ...
+        # request_text armazena todas as informacoes da requisicao GET /index.html HTTP/1.1, host, ...
         request_text = request_data.decode('utf-8')
 
         #Caso a requisição não seja GET, retornar um erro 502 Not Implemented
@@ -66,8 +66,9 @@ def handle_client(client_socket, base_dir):
             if os.path.exists(not_found_file_path):
                 with open(not_found_file_path, 'rb') as not_found_file:
                     not_found_content = not_found_file.read()
-            # Enviar a resposta com o conteúdo de NotFound.html
+            # Formata a resposta desejada como 400
             response = f"HTTP/1.1 404 Not Found\r\nContent-Length: {len(not_found_content)}\r\n\r\n"
+            # Enviar a resposta com o conteúdo de NotFound.html pelo socket
             client_socket.send(response.encode() + not_found_content)
              # Registrar informações de log
             log_request(client_socket, request_text, 404,log_file='log.txt')
@@ -75,10 +76,19 @@ def handle_client(client_socket, base_dir):
 
         with open(caminho, 'rb') as file:
             content = file.read()
+            # Formata a resposta desejada como 200
             response = f"HTTP/1.1 200 OK\r\nContent-Length: {len(content)}\r\n\r\n"
+            # Enviar a resposta com o conteúdo do arquivo pelo socket
             client_socket.send(response.encode() + content)
              # Registrar informações de log
             log_request(client_socket, request_text,200, log_file='log.txt')
+
+    # Tratar o timeout de 20 segundos
+    except socket.timeout:
+        print("Timeout de 20 segundos ocorreu. Encerrando a conexão.")
+        # Fechar o socket do cliente se houver timeout
+        client_socket.close()
+
     finally:
         client_socket.close()
 
@@ -86,16 +96,16 @@ def log_request(client_socket, request_text, status, log_file):
     # Obter o timestamp atual
     timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
 
-    # Obter o endereço IP do cliente
+    # Obter o endereço IP do cliente (que é o destino que o socket está se comunicando)
     client_ip = client_socket.getpeername()[0]
 
-    # Obter o código do resultado (que você precisa determinar com base na solicitação)
 
     # Formatar a entrada de log
     log_entry = f"{timestamp} - {client_ip} - [{status}] -{request_text}"
 
     # Bloquear o acesso ao arquivo de log para evitar problemas de requisicoes ao mesmo tempo
     with log_lock:
+        # Escrever a entrada de log no arquivo de log -> 'a' signidica que será escrito no arquivo
         with open(log_file, 'a') as log:
             log.write(log_entry + '\n')
 
@@ -108,6 +118,8 @@ def main():
     args = parser.parse_args()
 
     # Criar um socket TCP/IP
+    #AF_INET = familia de enderecos IPv4
+    #SOCK_STREAM = socket de fluxo TCP
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Impedir o erro "Address already in use"
@@ -118,6 +130,9 @@ def main():
     print(f"Servidor HTTP está ouvindo na porta 8080, servindo arquivos de {args.base_dir}")
 
     while True:
+        # Aguardar por novas conexões
+        #client_socket = socket do cliente
+        #client_address = endereço do cliente
         client_socket, client_address = server_socket.accept()
         print(f"Conexão de {client_address}")
 
